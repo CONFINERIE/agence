275a3b3bb79ef5b1c98121fbc4584e7ee0438546
CONFINERIE <dannyconfi020@gmail.com>
2024-07-19 07:20:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar');
+const closeBtn = document.querySelector('.close-btn');
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    navbar.classList.remove('active');
 });
